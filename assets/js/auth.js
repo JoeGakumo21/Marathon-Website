5b64147a50974980cea2132c8da0ee495b58ce5e
Joe Gakumo <joegakumo1@gmail.com>
2021-08-04 15:39:55 +0300

    add new object function

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -21,7 +21,22 @@ auth.onAuthStateChanged(user=>{
     }
 
 });
+// create new Guides
+const createForm=document.querySelector("#create-form");
+createForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
 
+    db.collection("marathonSports").add({
+       title:createForm["title"].value,
+       content:createForm["content"].value
+    }).then(()=>{
+        // close the modal 
+        const modal=document.querySelector("#modal-create");
+        // use materilize for modal
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    });
+});
 
 
 // sign up
@@ -55,7 +70,7 @@ const logout=document.querySelector("#logout");
 logout.addEventListener("click", (e)=>{
     e.preventDefault();
     auth.signOut().then(()=>{
-        // alert("Successfully Logged out ");
+        confirm("About to Log out ");
     });
 });
 
@@ -72,7 +87,7 @@ loginForm.addEventListener("submit", (e)=>{
     // get details from firebase
     auth.signInWithEmailAndPassword(email, password).then(cred=>{
         // console.log(cred.user);
-
+        // alert("successifully logged in")
         // close the login modal and reset the form
         const modal=document.querySelector("#modal-login");
         // use materilize for modal
